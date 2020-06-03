8f239f05f30d8357a2d4eef92157c104d8f52004
Diego Prestes <diego.prestes@possible.com>
2020-06-03 17:12:39 -0300

    added some animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,19 @@ const Home = () => {
   return (
     <motion.div
       className="home container"
-      animate={{}}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
     >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{}}>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: '0px 0px 8px rgb(255, 255, 255)',
+            boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+          }}
+        >
           Create Your Pizza
         </motion.button>
       </Link>
